2ec548cb5930058baa9152743640e8e183824496
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-02-28 22:24:21 +0530

    Update sample_config.py

--- SungJinwooRobot/sample_config.py
+++ SungJinwooRobot/sample_config.py
@@ -15,12 +15,12 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
+    API_ID = 2091894  # integer value, dont use ""
+    API_HASH = "925ce96bd585205d6dac1b35e8e26a12"
     TOKEN = "1616584987:AAGiKBL-7Q4mihWH03AGtyBjfDNdp23zJVc"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 769830161 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "HIRO"
-    SUPPORT_CHAT = 'KURUMI SUPPORT DAMN'  #Your own group for support, do not add the @
+    SUPPORT_CHAT = 'kurumisupport'  #Your own group for support, do not add the @
     JOIN_LOGGER = -1001151528469  #Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = -1001151528469  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
