d0f9b6f92d40b4b5338c819e2fae3dd522fac6e1
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-03-02 12:18:42 +0530

    Update afk.py

--- SungJinwooRobot/modules/afk.py
+++ SungJinwooRobot/modules/afk.py
@@ -36,7 +36,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(
+        update.effective_message.reply_text("{} is now dead!{}".format(
             fname, notice))
     except BadRequest:
         pass
@@ -57,7 +57,7 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} is here!', '{} is back!', '{} is now in the chat!',
+                '{} You were reading Still didn't message till now Why!?', '{} is here!', '{} is back!', '{} is now in the chat!',
                 '{} is awake!', '{} is back online!', '{} is finally here!',
                 'Welcome back! {}', 'Where is {}?\nIn the chat!'
             ]
