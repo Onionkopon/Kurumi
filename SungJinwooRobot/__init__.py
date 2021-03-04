1936e942a8ae1cb9ffec4d53758fb761ca6bb3eb
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-03-04 18:02:50 +0530

    Update __init__.py

--- SungJinwooRobot/__init__.py
+++ SungJinwooRobot/__init__.py
@@ -184,7 +184,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from Kurumi.modules.helper_funcs.handlers import (CustomCommandHandler,
+from SungJinwooRobot.modules.helper_funcs.handlers import (CustomCommandHandler,
                                                         CustomMessageHandler,
                                                         CustomRegexHandler)
 
