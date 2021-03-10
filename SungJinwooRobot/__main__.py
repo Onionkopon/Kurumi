91cd3ede00ae4aa688892876aab698eecb5debf0
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-03-10 19:12:18 +0530

    Update __main__.py

--- SungJinwooRobot/__main__.py
+++ SungJinwooRobot/__main__.py
@@ -203,8 +203,8 @@ def start(update: Update, context: CallbackContext):
                              text="🚨 Support 🚨",
                              url=f"https://t.me/{SUPPORT_CHAT}"),
                          InlineKeyboardButton(
-                             text="My love ❤",
-                             url="https://t.me/A_lonelyPerson")
+                             text="🔗Source🔗",
+                             url="https://github.com/Hirojazz/Kurumi")
                     ], 
                      [
                          InlineKeyboardButton(
