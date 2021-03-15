d2c8ab44428a64eff17cea41597f778117194e8f
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-03-15 17:49:40 +0530

    Update meme.py

--- SungJinwooRobot/modules/meme.py
+++ SungJinwooRobot/modules/meme.py
@@ -16,30 +16,36 @@ GIF_ID = "CgACAgQAAx0CSVUvGgAC7KpfWxMrgGyQs-GUUJgt-TSO8cOIDgACaAgAAlZD0VHT3Zynpr
 
 
 @run_async
-def hug(update: Update, context: CallbackContext):
-    bot = context.bot
-    args = context.args
+def sanitize(update: Update, context: CallbackContext):
     message = update.effective_message
-
-    reply_to = message.reply_to_message if message.reply_to_message else message
-
-    curr_user = html.escape(message.from_user.first_name)
-    user_id = extract_user(message, args)
-
-    if user_id:
-        patted_user = bot.get_chat(user_id)
-        user1 = curr_user
-        user2 = html.escape(hugged_user.first_name)
-
-    else:
-        user1 = bot.first_name
-        user2 = curr_user
+    name = (
+        message.reply_to_message.from_user.first_name
+        if message.reply_to_message
+        else message.from_user.first_name
+    )
     reply_animation = (
         message.reply_to_message.reply_animation
         if message.reply_to_message
         else message.reply_animation
     )
-    reply_animation(random.choice(meme_strings.HUG_GIFS), caption=f"*{user1} hugs {user2}*")
+    reply_animation(GIF_ID, caption=f"*Sanitizes {name}*")
+
+
+@run_async
+def sanitize(update: Update, context: CallbackContext):
+    message = update.effective_message
+    name = (
+        message.reply_to_message.from_user.first_name
+        if message.reply_to_message
+        else message.from_user.first_name
+    )
+    reply_animation = (
+        message.reply_to_message.reply_animation
+        if message.reply_to_message
+        else message.reply_animation
+    )
+    reply_animation(random.choice(meme_strings.GIFS), caption=f"*Sanitizes {name}*")
+   
    
 #plugin by t.me/RCage
 @run_async
@@ -57,19 +63,15 @@ def meme(update: Update, context: CallbackContext):
 
 MEME_HANDLER = DisableAbleCommandHandler("meme", meme)
 SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize)
-HUG_HANDLER = DisableAbleCommadHandler("hug", hug) 
 
 dispatcher.add_handler(MEME_HANDLER)
 dispatcher.add_handler(SANITIZE_HANDLER)
-dispatcher.add_handler(HUG_HANDLER)
 
 __command_list__ = [
     "sanitize",
     "meme",
-    "hug",
 ]
 __handlers__ = [
     SANITIZE_HANDLER,
     MEME_HANDLER,
-    HUG_HANDLER,
 ]
