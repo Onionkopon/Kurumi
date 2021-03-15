59c32e21822ec3213f9cfb0a2b7a43690c5c01aa
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-03-15 16:03:25 +0530

    Update fun.py

--- SungJinwooRobot/modules/fun.py
+++ SungJinwooRobot/modules/fun.py
@@ -11,7 +11,7 @@ from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, run_async
 
-GIF_ID = 'CgACAgQAAx0CSVUvGgAC7KpfWxMrgGyQs-GUUJgt-TSO8cOIDgACaAgAAlZD0VHT3Zynpr5nGxsE'
+GIF_ID = "CgACAgQAAxkBAAILHWBPN8dL8NvxZ9tUfr3_4SdPGqgjAAJeAgACQQrNUlM24z1ISCsTHgQ"
 
 
 @run_async
